1aaa120400515419898a4322b390d1aa0519f5c2
Shreya Shristi <shristishreyac@gmail.com>
2025-12-06 17:05:41 +0530

    Frontend started

--- ExpenseTracker/Tracker/admin.py
+++ ExpenseTracker/Tracker/admin.py
@@ -1,10 +1,73 @@
 from django.contrib import admin
 from .models import Expense
+from django.http import HttpResponse
+import csv
+
 
 @admin.register(Expense)
 class ExpenseAdmin(admin.ModelAdmin):
+    """
+    Professional, production-ready admin configuration for Expense model.
+    """
+
+    # Columns to display in list view
     list_display = ('title', 'amount', 'category', 'date')
+
+    # Sidebar filters
     list_filter = ('category', 'date')
+
+    # Search bar fields
     search_fields = ('title', 'category')
-    ordering = ('-date',)  # descending order
-#Done the basic!
+
+    # Sort newest → oldest
+    ordering = ('-date',)
+
+    # created_at shouldn't be edited manually
+    readonly_fields = ('created_at',)
+
+    # Pagination for cleaner admin
+    list_per_page = 20
+
+    # Adds a date drill-down navigation bar at top
+    date_hierarchy = 'date'
+
+    # Organize form layout
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('title', 'amount', 'category')
+        }),
+        ('Additional Details', {
+            'fields': ('description', 'date')
+        }),
+        ('System Metadata', {
+            'fields': ('created_at',),
+            'classes': ('collapse',),  # collapsible clean UI
+        })
+    )
+
+    # Register custom actions
+    actions = ['export_selected_expenses']
+
+    def export_selected_expenses(self, request, queryset):
+        """
+        Export selected Expense records as CSV.
+        Common real-world admin action.
+        """
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=expenses_export.csv'
+
+        writer = csv.writer(response)
+        writer.writerow(['Title', 'Amount', 'Category', 'Date', 'Description'])
+
+        for expense in queryset:
+            writer.writerow([
+                expense.title,
+                expense.amount,
+                expense.category,
+                expense.date,
+                expense.description or ""
+            ])
+
+        return response
+
+    export_selected_expenses.short_description = "Export selected expenses as CSV"
--- ExpenseTracker/Tracker/views.py
+++ ExpenseTracker/Tracker/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Expense   # <-- REQUIRED import
 
-# Create your views here.
+def home(request):
+    return render(request, 'Tracker/home.html')
+
+def expense_list(request):
+    expenses = Expense.objects.all().order_by('-date')
+    return render(request, 'Tracker/expense_list.html', {'expenses': expenses})
